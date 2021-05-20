066e9a800f10f1b93641dc8b5c323d840ed0d7fd
Jesus Sanchez <jxasanchez@gmail.com>
2021-05-19 21:31:30 -0700

    update ports

--- app.js
+++ app.js
@@ -186,6 +186,12 @@ app.post("/login", (req, res) => {
     });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started.");
+let port = process.env.PORT;
+
+if(port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log("Server started successfully.");
 });
