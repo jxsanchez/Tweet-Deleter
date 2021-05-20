27a8d8b4179b8a99cffaecc2c20cf5de9dc7b91d
Jesus Sanchez <jxasanchez@gmail.com>
2021-05-20 07:09:29 -0700

    add character count and keychange handling on number input

--- public/deleter.js
+++ public/deleter.js
@@ -1,15 +1,53 @@
+const CHAR_LIMIT = 280;
 const MAX = 200;
 
+const tweetContent = $(".tweet-content");
+const charCount = $(".char-count");
+const tweetBtn = $(".tweet-btn");
 const likeDelInput = $(".like-del-num");
-
 const likeDelCheckbox = $(".like-del-checkbox");
 
+// Initialize character count.
+charCount.text(tweetContent.val().length);
+
+// Character count update function
+tweetContent.on("change keyup keypress", () => {
+    // Store number of characters in tweet
+    count = tweetContent.val().length;
+
+    // Set number in character count
+    charCount.text(count);
+
+    // Change character count font color based on number of characters
+    if(count == 0) {
+        charCount.css("color", "#657786");
+        tweetBtn.prop("disabled", true);
+    } else if (count > 0 && count < 280) {
+        charCount.css("color", "#35a824");
+        tweetBtn.prop("disabled", false);
+    } else {
+        charCount.css("color", "#f52e14");
+    }
+});
+
+// Set number of likes to be deleted by checking and unchecking the checkbox
 likeDelCheckbox.click(() => {
+    // Set number input to 1 when unchecked, else set to MAX
     if(likeDelCheckbox.attr("checked")) {
-        likeDelCheckbox.attr("checked", false)
+        likeDelCheckbox.attr("checked", false);
         likeDelInput.val(1);
     } else {
-        likeDelCheckbox.attr("checked", true)
+        likeDelCheckbox.attr("checked", true);
         likeDelInput.val(MAX);
+    }
+});
+
+// Detect changes in number input to check and uncheck 
+likeDelInput.on("change keyup keypress", () => {
+    // Uncheck checkbox if number is below MAX, else check 
+    if(likeDelInput.val() < MAX) {
+        likeDelCheckbox.attr("checked", false);
+    } else {
+        likeDelCheckbox.attr("checked", true);
     }
 });
