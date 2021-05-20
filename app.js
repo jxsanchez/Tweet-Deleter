c950f741df6d7be432fbd04e06b1b0d9bf44a4db
Jesus Sanchez <jxasanchez@gmail.com>
2021-05-19 22:32:00 -0700

    clean up app.js, remove unnecessary requests

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ const session = require("express-session");
 const TwitterStrategy = require("passport-twitter").Strategy;
 const twit = require("./twit");
 
-
+// Create app object using express
 const app = express();
 
-app.use(express.static("public")); // Use static files in public folder for styling
+/*********** 
+ * app.use *
+ ***********/
+// Use static files in public folder
+app.use(express.static("public"));
+
+// Use body-parser to get info from requests 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -23,17 +29,19 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
-app.use(passport.initialize()); // Initialize passport
-app.use(passport.session()); // Use passport to manage session
 
+// Initialize passport and use passport to manage session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set view engine to ejs to allow for creation of template files
 app.set("view engine", "ejs");
 
+// Connect mongoose to remote database
 mongoose.connect("mongodb+srv://jesus-admin:" + process.env.MONGODB_PASSWORD+ "@like-deleter.moznz.mongodb.net/userDB", { useNewUrlParser : true, useUnifiedTopology: true });
 mongoose.set("useCreateIndex", true);
 
-const saltRounds = 5;
-
-// User schema
+// User schema for creating new users
 const userSchema = new mongoose.Schema({
     twitterId: String,
     username: String,
@@ -42,26 +50,31 @@ const userSchema = new mongoose.Schema({
     tokenSecret: String
 });
 
-// Use to hash and salt passwords
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
+// mongoose Schema plugins
+userSchema.plugin(passportLocalMongoose); // Used for logining in with password
+userSchema.plugin(findOrCreate); // Finds or creates user
 
-// User model
+// Creates User model
 const User = new mongoose.model("User", userSchema);
 
 /***********************
  * PASSPORT STRATEGIES *
  ***********************/
-passport.use(User.createStrategy()); // Create local login strategy
+// Create local login strategy
+passport.use(User.createStrategy()); 
 
-passport.use(new TwitterStrategy({ // Create Twitter strategy
+// Create Twitter strategy
+passport.use(new TwitterStrategy({ 
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: "https://thawing-forest-99001.herokuapp.com/auth/twitter/deleter"
+    /* callbackURL: "https://thawing-forest-99001.herokuapp.com/auth/twitter/deleter" */
+    callbackURL: "http://127.0.0.1:3000/auth/twitter/deleter"
   },
   function(token, tokenSecret, profile, cb) {
+    // Get full-sized profile photo
     const imgUrl = (profile.photos[0].value).replace("_normal", "");
 
+    // Find user based on criteria from profile JSON object
     User.findOrCreate({ twitterId: profile.id, username: profile.username, profileImgUrl: imgUrl, token: token, tokenSecret: tokenSecret}, function (err, user) {
       return cb(err, user);
     });
@@ -84,33 +97,29 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/register", (req, res) => {
-    res.render("register");
-});
-
-app.get("/login", (req, res) => {
-    res.render("login");
-});
-
+// passport-twitter authentication route
 app.get('/auth/twitter', passport.authenticate('twitter'));
 
+// Redirects user to deleter page once successfully logged in
 app.get('/auth/twitter/deleter', 
   passport.authenticate('twitter', { failureRedirect: '/login' }),
   function(req, res) {
-    // Successful authentication, redirect home.
     res.redirect('/deleter');
 });
 
 app.get("/deleter", (req, res) => {
+    // Renders the deleter page if the user is successfully authenticated
     if(req.isAuthenticated()) {
         const { username, profileImgUrl } = req.user ;
 
         res.render("deleter", { username: username, profileImgUrl: profileImgUrl });
+    // Redirects the user to the home page if they try to access the deleter page without logging in
     } else {
         res.redirect("/");
     }
 });
 
+// Logs the user out, destroys cookies, and redirects to home
 app.get("/logout", (req, res) => {
     req.logout();
     
@@ -121,13 +130,15 @@ app.get("/logout", (req, res) => {
  * POST Requests *
  *****************/
 app.post("/tweet", (req, res) => {
+    // Configure twit object
     const T = twit.twitConfig(req);
       
-      T.post("statuses/update", { status: req.body.tweet }, function(err, data, response) {
+    // Sends tweet with information provided from tweet form and redirects to deleter page
+    T.post("statuses/update", { status: req.body.tweet }, function(err, data, response) {
         if(!err) {
             res.redirect("/deleter");   
         }
-      });
+    });
 });
 
 app.post("/delete-likes", (req, res) => {
@@ -139,11 +150,9 @@ app.post("/delete-likes", (req, res) => {
         // Delete each like
         data.forEach(like => {
             T.post("favorites/destroy", { id: like.id_str }, (err, data, response) => {
-                if(!err) {
-                    console.log(data);
-                } else {
-                    console.log(response.statusCode);
-                    console.log("could not delete like");
+                // Redirect user to deleter if there is an error deleting a tweet
+                if(err) {
+                    res.redirect("/deleter");
                 }
             });
         });
@@ -152,46 +161,15 @@ app.post("/delete-likes", (req, res) => {
     });
 });
 
-// Handles POST request from /register form
-app.post("/register", (req, res) => {
-    // Passport register function
-    User.register({ username: req.body.username }, req.body.password, (err, user) => {
-        if(err) {
-            res.redirect("/register");
-        } else {
-            // Authenticate user and redirect to deleter
-            passport.authenticate("local")(req, res, () => {
-                res.redirect("/deleter");
-            });
-        }
-    });
-});
-
-// Handles POST request from /login form
-app.post("/login", (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
-
-    req.login(user, err => {
-        if(err) {
-            res.redirect("/login");
-        } else {
-            // Authenticate user and redirect to deleter
-            passport.authenticate("local")(req, res, () => {
-                res.redirect("/deleter");
-            });
-        }
-    });
-});
-
+// Get port from process (running on web server)
 let port = process.env.PORT;
 
+// Uses port 3000 if running on local server
 if(port == null || port == "") {
     port = 3000;
 }
 
+// Starts the server
 app.listen(port, () => {
     console.log("Server started successfully.");
 });
