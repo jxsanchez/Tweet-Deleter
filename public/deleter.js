7af8c36da6873c3a6e93225198c27975c6bfc323
Jesus Sanchez <jxasanchez@gmail.com>
2021-05-20 20:12:04 -0700

    disable tweet button on 0 chars

--- public/deleter.js
+++ public/deleter.js
@@ -10,8 +10,12 @@ const likeDelCheckbox = $(".like-del-checkbox");
 // Initialize character count.
 charCount.text(tweetContent.val().length);
 
+// Disable tweet button
+tweetBtn.addClass("disabled");
+tweetBtn.prop("disabled", true);
+
 // Character count update function
-tweetContent.on("change keyup keypress", () => {
+tweetContent.on("input", () => {
     // Store number of characters in tweet
     count = tweetContent.val().length;
 
@@ -21,12 +25,16 @@ tweetContent.on("change keyup keypress", () => {
     // Change character count font color based on number of characters
     if(count == 0) {
         charCount.css("color", "#657786");
+        tweetBtn.addClass("disabled");
         tweetBtn.prop("disabled", true);
     } else if (count > 0 && count < 280) {
         charCount.css("color", "#35a824");
+        tweetBtn.removeClass("disabled");
         tweetBtn.prop("disabled", false);
     } else {
         charCount.css("color", "#f52e14");
+        tweetBtn.removeClass("disabled");
+        tweetBtn.prop("disabled", false);
     }
 });
 
@@ -34,20 +42,25 @@ tweetContent.on("change keyup keypress", () => {
 likeDelCheckbox.click(() => {
     // Set number input to 1 when unchecked, else set to MAX
     if(likeDelCheckbox.attr("checked")) {
+        console.log("is checked, unchecking and changing val to 1");
         likeDelCheckbox.attr("checked", false);
         likeDelInput.val(1);
     } else {
+        console.log("NOT checked, checking and changing val to 200");
         likeDelCheckbox.attr("checked", true);
         likeDelInput.val(MAX);
     }
 });
 
 // Detect changes in number input to check and uncheck 
-likeDelInput.on("change keyup keypress", () => {
+likeDelInput.on("input", () => {
+    console.log("like del input value changed");
     // Uncheck checkbox if number is below MAX, else check 
     if(likeDelInput.val() < MAX) {
+        console.log("value less than max, unchecking");
         likeDelCheckbox.attr("checked", false);
     } else {
+        console.log("value IS max, checking box");
         likeDelCheckbox.attr("checked", true);
     }
 });
